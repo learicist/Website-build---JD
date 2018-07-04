54ecd3973ceea433fb00feadf0e7b2d514bc1b76
The Doctor <learicist@gmail.com>
2018-07-04 01:25:01 -0400

    color scheme change, pics add, formatting, form functionality

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const buttons = {
 
 
 $(document).ready(function () {
-	
+	console.log('Hello Jenna');
 	//css for the buttons upon mouse enter
-	/*$('.btn').mouseenter(function (event) {
+	/*$('.main').mouseenter(function (event) {
 		//event.preventDefault();
 		$(this).css('background-color', 'black');
 		$(this).css('color', 'aqua');
@@ -29,29 +29,30 @@ $(document).ready(function () {
 		}
 	});
 
-	$('.btn').on('click', function () {
-		
-		//reassign hidden class to currently active window state
-		for (let i in buttons) {
-			if (buttons[i] !== buttons[this.id]) {
-				buttons[i].addClass('hidden');
-			}			
+	$('.main').on({
+		'mouseenter': function () {
+			$(this).css({
+				'background-color': '#c2c1c9',
+				'color': '#000'
+			});
+		},
+		'mouseleave': function () {
+			$(this).css({
+				'background-color': '#5a5866',
+				'color': '#f9f9fa'
+			});
+		},
+		'click': function () {
+			for (let i in buttons) {
+				if (buttons[i] !== buttons[this.id]) {
+					buttons[i].addClass('hidden');
+				}			
+			}
+			buttons[this.id].removeClass('hidden');
+			//buttons[this.id].removeClass('hidden');
+			//buttons[userArray[userArray.length - 1]].addClass('hidden');*/
+			console.log(this.id);
 		}
-		
-		buttons[this.id].removeClass('hidden');
-		
-		/*let curr = userArray.splice(-1);
-		buttons[this.id].removeClass('hidden');
-		buttons[curr].addClass('hidden');
-		userArray.push(this.id);
-		LAST ITEM IN userArray needs to be reassgined the class of hidden
-		//let selector = '$("#' + this.id + 'Summon")';        //`$('#${this.id}Summon')`;
-		//console.log(selector);
-		//MAKE CONDITION FOR if the button clicked isn't the page you're already on
-		//buttons[this.id].removeClass('hidden');
-		//buttons[userArray[userArray.length - 1]].addClass('hidden');*/
-		
-		console.log(this.id);
 	});
 	
 	/* .on('click', "#pics", function () {
