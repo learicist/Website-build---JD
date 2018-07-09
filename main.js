d2c83cacf4b33c9ef69657ce81a4f29a31c3e812
The Doctor <learicist@gmail.com>
2018-07-08 22:55:24 -0400

    changes to color scheme, functionality, playback stop on reel

--- main.js
+++ main.js
@@ -6,19 +6,8 @@ const buttons = {
 	'drawing': $('#drawingSummon')
 }
 
-
 $(document).ready(function () {
 	console.log('Hello Jenna');
-	//css for the buttons upon mouse enter
-	/*$('.main').mouseenter(function (event) {
-		//event.preventDefault();
-		$(this).css('background-color', 'black');
-		$(this).css('color', 'aqua');
-	}).mouseleave(function (event) {
-		//event.preventDefault();
-		$(this).css('background-color', 'aqua');
-		$(this).css('color', 'black');
-	});*/
 	
 	$('#title').on({
 		'mouseenter': function () {
@@ -28,39 +17,35 @@ $(document).ready(function () {
 			location.reload();
 		}
 	});
-
+	
 	$('.main').on({
 		'mouseenter': function () {
 			$(this).css({
-				'background-color': '#c2c1c9',
-				'color': '#000'
+				'background-color': '#4b4a55',
+				'color': '#ebeaed'
 			});
 		},
 		'mouseleave': function () {
 			$(this).css({
-				'background-color': '#5a5866',
-				'color': '#f9f9fa'
+				'background-color': '#ebeaed',
+				'color': '#4b4a55'
 			});
 		},
 		'click': function () {
+			//stop video if user leaves screen
+			if (!buttons.reelBtn.hasClass('hidden')) {
+				let temp = $('#reel').attr('src'); 
+				$('#reel').attr('src', temp);
+			}
+			
 			for (let i in buttons) {
 				if (buttons[i] !== buttons[this.id]) {
 					buttons[i].addClass('hidden');
 				}			
 			}
 			buttons[this.id].removeClass('hidden');
-			//buttons[this.id].removeClass('hidden');
-			//buttons[userArray[userArray.length - 1]].addClass('hidden');*/
 			console.log(this.id);
 		}
-	});
-	
-	/* .on('click', "#pics", function () {
-		
-		$('#drawing').hide(1500);
-		$('#picsSummon').removeClass('hidden');
-		
-	});*/
-	
+	});	
 	
 });
