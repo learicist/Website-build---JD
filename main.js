78a5a17c64a73f4cf4868c8c57592db5cd2bd56a
The Doctor <learicist@gmail.com>
2018-11-10 23:47:25 -0500

    Implement new set of requests, session 2.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const buttons = {
 	'picsBtn': $('#picsSummon'),
 	'linksBtn': $('#linksSummon'),
 	'contactBtn': $('#contactSummon'),
-	'drawing': $('#drawingSummon')
+	'resBtn': $('#resSummon')
 }
 
 $(document).ready(function () {
@@ -21,19 +21,37 @@ $(document).ready(function () {
 		},
 		
 		'click': function () {
+			//set background to black
+			$('body').css('background', 'black');
+			
+			//reposition name div 
+			$('#jenna')
+				.prependTo('#topRow')
+				.css({'padding-left': '15px', 'padding-top': '5px'});
+			
+			//change name to white
+			$('#title').css({
+				'color': 'white',
+				'font-size': '1.15em',
+				'font-weight': '400',
+				'letter-spacing': '0',
+				'opacity': '0.9'
+			});
+			
 			//stop video if user leaves screen
 			if (!buttons.reelBtn.hasClass('hidden')) {
 				let temp = $('#reel').attr('src'); 
 				$('#reel').attr('src', temp);
 			}
 			
+			//display chosen page and hide others
 			for (let i in buttons) {
 				if (buttons[i] !== buttons[this.id]) {
 					buttons[i].addClass('hidden');
 				}			
 			}
 			buttons[this.id].removeClass('hidden');
-			console.log(this.id);
+			//console.log(this.id);
 		}
 	});	
 	
